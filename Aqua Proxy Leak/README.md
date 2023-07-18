16c713ad79216a9afd74f160be5ce29aabc88523
Anowez <138973301+ThePersonWhoNamedAnowez@users.noreply.github.com>
2023-07-18 11:52:08 +0300

    some kid tried ratted me and i leak theirs proxy
    dont download the proxy named aqua i already leak it :D

---
+++ Aqua Proxy Leak/README.md
@@ -0,0 +1,3 @@
+# AJProxy leaked source
+owner skids from other sources zzz
+gay idiot snitched me over 13dl
---
+++ Aqua Proxy Leak/proxy/authentication.hpp
@@ -0,0 +1,9 @@
+/*#pragma once
+#include <Windows.h>
+#include "auth.hpp"
+#include <string>
+
+namespace authentication {
+	bool login(std::string username);
+	bool createaccount(std::string username, std::string license);
+}*/
