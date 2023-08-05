49b1f5ddd5e38fde5bd9f5a0be1f6b4e47a487f9
Anowez <138973301+ThePersonWhoNamedAnowez@users.noreply.github.com>
2023-08-05 15:22:58 +0300

    Delete Aqua Proxy Leak directory

--- Aqua Proxy Leak/README.md
+++
@@ -1,3 +0,0 @@
-# AQUA Proxy leaked source
-Dont give any dls i leaked it
-its free now :)
--- Aqua Proxy Leak/proxy/authentication.hpp
+++
@@ -1,9 +0,0 @@
-/*#pragma once
-#include <Windows.h>
-#include "auth.hpp"
-#include <string>
-
-namespace authentication {
-	bool login(std::string username);
-	bool createaccount(std::string username, std::string license);
-}*/
